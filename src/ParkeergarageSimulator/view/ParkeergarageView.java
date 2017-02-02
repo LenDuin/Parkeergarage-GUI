e5fe81ffded37ec6e90b3f38e634ee6e2aaacdd9
mboonstra <mp.boonstra@live.nl>
2017-02-02 12:44:10 +0100

    ParkeergarageView werkend, nog steeds een vreemde weergave met de controllers, maar we hebben de functionaliteit van het origineel erin zitten.

--- src/ParkeergarageSimulator/view/ParkeergarageView.java
+++ src/ParkeergarageSimulator/view/ParkeergarageView.java
@@ -20,22 +20,28 @@ public class ParkeergarageView extends AbstractView{
             return;
         }
 
-        int margin = 100 - state.length * 2;
+        int hmargin = 100 - state.length * 2;
+        int vmargin = 100 - state.length * 2;
         for (int i = 0; i < state.length; i++) {
             for (int j = 0; j < state[i].length; j++) {
+                if(i>0 && j==0) {
+                    hmargin += 150;
+                }
+                if(j%2 == 0) {
+                    hmargin += 20;
+                }
                 for (int k = 0; k < state[i][j].length; k++) {
                     if (state[i][j][k] == null) {
                         g.setColor(Color.WHITE);
-                        g.fillRect(margin+20*i,margin+20*j,15,15);
                     } else {
                         if (state[i][j][k].getHasToPay()) {
                             g.setColor(Color.RED);
-                            g.fillRect(margin + 20 * i, margin + 20 * j, 15, 15);
                         } else {
                             g.setColor(Color.BLUE);
-                                g.fillRect(margin + 20 * i, margin + 20 * j, 15, 15);
                         }
                     }
+
+                    g.fillRect(hmargin + 20*j, vmargin + 11*k, 15, 10);
                 }
             }
         }
